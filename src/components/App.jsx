daf94306c010dceec733d5c58800794c18183c23
Markov Dmitriy Yurevich <d.markov@burdamedia.vhbk>
2023-03-09 21:47:55 +0200

    attemptRe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,33 @@
-import Phonebook from 'components/Phonebook/Phonebook';
-import { Provider } from 'react-redux';
-import { store } from 'redux/store';
+import { useEffect, lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+// import { PrivateRoute } from './PrivateRoute';
+// import { RestrictedRoute } from './RestrictedRoute';
+// import { refreshUser } from 'redux/auth/operations';
+// import { useAuth } from 'hooks';
+
+const HomePage = lazy(() => import('pages/Home'));
+const RegisterPage = lazy(() => import('pages/Register'));
+const LoginPage = lazy(() => import('pages/Login'));
+const ContactsPage = lazy(() => import('pages/Contacts'));
 
 export const App = () => {
+  // const dispatch = useDispatch();
+  // const { isRefreshing } = useAuth();
+
+  // useEffect(() => {
+  //   dispatch(refreshUser());
+  // }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Provider store={store}>
-        <Phonebook />
-      </Provider>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/tasks" element={<ContactsPage />} />
+      </Route>
+    </Routes>
   );
 };
